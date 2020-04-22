f1c727cd9cd0399e76f9d8cfa46f49ddb03f2e11
wynonazhou <44531222+wynonazhou@users.noreply.github.com>
2020-04-22 15:32:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,14 @@ function addToDo(toDo){
 
 addToDo("Collect & Sell 10 Shells")
 
+// add an item to the list user the enter key
+document.addEventListener("keyup",function(even){
+    if(event.keyCode == 13){
+        const toDo = input.value;
+
+        //if the input isn't empty
+        if(toDo){
+            addToDo(toDo);
+        }
+    }
+});
